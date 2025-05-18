cd2756cb3ed0a00cbabe2264838f909601d33020
ErbetoMiheretab <mengeshamiheretab36@gmail.com>
2025-05-18 11:25:23 +0300

    Add Trigram extension migration and enhance search functionality with ranking

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector,  SearchQuery, SearchRank
 from .forms import CommentForm, EmailPostForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-
+from django.contrib.postgres.search import TrigramSimilarity
 
 # Create your views here.
 
@@ -141,9 +141,18 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body")
-            ).filter(search=query)
+            search_vector = SearchVector("title", weight="A") + SearchVector(
+                "body", weight="B"
+            )
+            search_query = SearchQuery(query, config="english")
+            results = (
+                Post.published.annotate(
+                    search=search_vector, rank=SearchRank(search_vector, search_query),
+                    similarity=TrigramSimilarity('title', query),
+                )
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
+            )
 
     return render(
         request,
