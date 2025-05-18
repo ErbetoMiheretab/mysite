d825ab3e09682f33f9c160c4b468e509e5731857
ErbetoMiheretab <mengeshamiheretab36@gmail.com>
2025-05-18 11:05:13 +0300

    Add search functionality for blog posts with form and template integration

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import CommentForm, EmailPostForm
+from django.contrib.postgres.search import SearchVector
+from .forms import CommentForm, EmailPostForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
@@ -11,44 +12,39 @@ from django.db.models import Count
 
 # Create your views here.
 
+
 class PostListView(ListView):
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
+
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
-    tag=None
+    tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 3)
-    page_number= request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(
-        request,
-        'blog/post/list.html',
-        {
-            'posts': posts,
-            'tag': tag
-            }
-    )
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
-def post_detail(request, year, month, day , post):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day
+        publish__day=day,
     )
     comments = post.comments.filter(active=True)
 
@@ -56,24 +52,22 @@ def post_detail(request, year, month, day , post):
 
     # List of similar posts
 
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(
-        tags__in=post_tags_ids
-    ).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(
-        same_tags=Count('tags')
-    ).order_by('-same_tags', '-publish')[:4]
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
 
     return render(
         request,
-        'blog/post/detail.html',
-        {'post': post,
-         'comments': comments,
-         'form': form,
-         'similar_posts': similar_posts
-         }
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
-
 
 
 def post_share(request, post_id):
@@ -122,11 +116,7 @@ def post_share(request, post_id):
 
 @require_POST
 def post_comment(request, post_id):
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
 
     form = CommentForm(data=request.POST)
@@ -139,4 +129,24 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body")
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "taggit",
     "blog.apps.BlogConfig",
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
